1194e1ced62fcb42486cc9ba1bc4bad75e9988ea
Héctor <hecmocer@gmail.com>
2016-02-29 12:45:35 +0100

    Quitado header de formulario, refactorizado un poco el codigo javascript y añadida funcion ajax. Ahora cuando se hace submit, se envian los datos al servidor

--- javascript/formulario.js
+++ javascript/formulario.js
@@ -1,10 +1,15 @@
 
 $("form").on("submit", function(){
 
+    // Campos del formulario
     var artista = $.trim( $("#artista").val() );
     var titulo = $.trim( $("#titulo").val() );
     var url = $.trim( $("#url").val() );
 
+    // Patrón para la url
+    var pattern = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/ig
+
+    // Validación de campos
     if(artista == ""){
         alert("El artista no puede ser vacío");
         return false;
@@ -13,17 +18,28 @@ $("form").on("submit", function(){
         alert("El titulo no puede ser vacío");
         return false;
     }
-
-    var pattern = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/ig
     if(url == "" && pattern.test(url) == false){
         alert("La URL de la carátula no es válida");
         return false;
     }
 
-    //alert(artista);
-    //alert(titulo);
-    //alert(url);
-    alert("ENVIO DE FORMULARIO CORRECTO");
+    // Petición ajax al servidor (no olvidar ejecutar server.py)
+    $.ajax({
+        method: 'POST',
+        url: "/api/canciones/",
+        data: JSON.stringify({
+            artista: artista,
+            titulo: titulo,
+            url: url,
+        }),
+        contentType: 'application/json',
+        success: function(){  // Success function
+            alert("Guardado con éxito!");
+        },
+        error: function(){  // Error function
+            alert("Se ha producido un error");
+        }
+    });
     return false; // Jquery cancela el envio del formulario (prevent default)
 
 });
