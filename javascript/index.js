7b6851f2873e5e811e019fac99533de4f0abf7b0
Jorge González <jorge.gonzalez@babel.es>
2016-03-01 13:15:37 +0100

    Añadidas funcionalidades de reproducción y pausa de canciones.

--- javascript/index.js
+++ javascript/index.js
@@ -23,9 +23,9 @@ $(document).ready(function() { // Cuando la página se ha cargado por completo
                     html += "</div>";
                     html += "<div class='col-xs-6'>";
                     html += "<div class='control-buttons'>";
-                    html += "<button data-songid='"+ id + "' class='glyphicon glyphicon-play play-button'></button>";
-                    html += "<button data-songid='"+ id + "' class='glyphicon glyphicon-pencil edit-button'></button>";
-                    html += "<button data-songid='"+ id + "' class='glyphicon glyphicon-trash delete-button'></button>";
+                    html += "<button data-songid='" + id + "' class='glyphicon glyphicon-play play-button'></button>";
+                    html += "<button data-songid='" + id + "' class='glyphicon glyphicon-pencil edit-button'></button>";
+                    html += "<button data-songid='" + id + "' class='glyphicon glyphicon-trash delete-button'></button>";
                     html += "</div>";
                     html += "</div>";
                     html += "</article>";
@@ -38,8 +38,27 @@ $(document).ready(function() { // Cuando la página se ha cargado por completo
         });
     } //loadSongs()
 
+    // Cargar nueva cancion
+
+    function loadSong(data) {
+        var html = "";
+        html += "<source src='" + data.url + "'>";
+        $('#mediaPlayer').html(html); // asigna la source de la cancion
+        $('#mediaPlayer').trigger('load'); // carga la cancion
+    }
+
+    function playSong(button) {
+        console.log("Reproduciendo cancion");
+        $('#mediaPlayer').trigger('play'); // reproduce la cancion
+        // cambiar la clase de los demas iconos a play
+        $('.pause-button').addClass('play-button glyphicon-play').removeClass('pause-button glyphicon-pause');
+        // cambiar manejador a traves de la clase
+        $(button).removeClass('play-button glyphicon-play').addClass('pause-button glyphicon-pause');
+    }
+
     // Manejadores de evento
 
+    // Boton de reproducción
     $("main").on('click', '.play-button', function() {
         var self = this; // this referencia al elemento del DOM button
         var id = $(self).data("songid");
@@ -49,12 +68,19 @@ $(document).ready(function() { // Cuando la página se ha cargado por completo
             url: '/api/canciones/' + id,
             type: 'get',
             success: function(data) {
-                console.log("Reproduciendo cancion");
-                var html = "";
-                html += "<source src='" + data.url + "'>"
-                $('#mediaPlayer').html(html); // asigna la source de la cancion
-                $(self).trigger('play');
-                // cambiar manejador
+                // si el reproductor no esta reproduciendo ninguna cancion
+                if (paused == null) {
+                    loadSong(data);
+                    playSong(self);
+                } else {
+                    if (paused == data.id) { // si ya esta cargada la cancion actual
+                        playSong(self);
+                    } else { // si hay que sobreescribir la cancion
+                        loadSong(data);
+                        paused = data.id; // nueva cancion
+                        playSong(self);
+                    }
+                }
             },
             error: function(data) {
                 console.log("Error al reproducir la canción", data);
@@ -62,9 +88,18 @@ $(document).ready(function() { // Cuando la página se ha cargado por completo
         });
     })
 
-    $(".stop-button")
+    // Botón de pausa
+    $("main").on('click', '.pause-button', function() {
+        console.log("Estableciendo manejador de stop");
+        $('#mediaPlayer').trigger('pause');
+        paused = $(this).data().songid;
+        console.log(paused);
+        $(this).removeClass('pause-button glyphicon-pause').addClass('play-button glyphicon-play');
+    })
 
     // Ejecución
+
+    var paused = null;
 
     loadSongs();
 
