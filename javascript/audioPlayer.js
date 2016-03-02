45b1f3078acbbab464cfd9cdf60f9a519fd3d914
Héctor <hecmocer@gmail.com>
2016-03-02 12:25:45 +0100

    Maquetado de la barra de reproduccion terminada

--- javascript/audioPlayer.js
+++ javascript/audioPlayer.js
@@ -1,13 +1,39 @@
-$("#playButtonFooter").on("click", function(){
-    var music = $("#mediaPlayer")[0];
+$(document).ready(function() {
 
-    if (music.paused) {
-        music.play();
-        $(this).removeClass('pauseButtonFooter glyphicon-pause').addClass('playButtonFooter glyphicon-play')
-        //console.log("Play");
-    } else {
-        music.pause();
-        $(this).removeClass('playButtonFooter glyphicon-play').addClass('pauseButtonFooter glyphicon-pause')
-        //console.log("Pause");
-    }
+    $("#playButtonFooter").on("click", function(){
+        var music = $("#mediaPlayer")[0];
+
+        if (music.paused) {
+            music.play();
+            $(this).removeClass('glyphicon-pause').addClass('glyphicon-play')
+            //console.log("Play");
+        } else {
+            music.pause();
+            $(this).removeClass('glyphicon-play').addClass('glyphicon-pause')
+            //console.log("Pause");
+        }
+    })
+
+    var duration;
+
+
+    $("#mediaPlayer").on("timeupdate", function(){
+        var playPercent = 100 * ($(this)[0].currentTime / duration);
+        $("#playhead")[0].style.marginLeft = playPercent + "%";
+    });
+
+    $("#mediaPlayer").on("canplaythrough", function(){
+        duration = $(this)[0].duration;
+    });
+
+    $("#nextButtonFooter").on("click", function(){
+
+        //$(this).data("songid", )
+
+
+    });
+
+    $("#prevButtonFooter").on("click", function(){
+
+    });
 })
